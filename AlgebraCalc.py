1190760904c915c736a49d71e8b5ddffd3b7d7a7
NabeelDaBoi <62586726+nabewpie@users.noreply.github.com>
2022-03-09 10:25:33 +0530

    Update #2
    Added an input system. Also shows the final equation according to the inputs. New name given: AlgCalc. 
    FLAW: The calculator has to have the signs specified or else the equation looks very weird.

--- AlgebraCalc.py
+++ AlgebraCalc.py
@@ -1,7 +1,14 @@
-term1_coef = -46
-term1_var = "c"
-term2_coef = -10
-term2_var = "b"
+print("AlgCalc v1.1")
+print("PLEASE SPECIFY THE SIGNS!!!")
+
+term1_coef = input("Term 1 coefficient: ")
+term1_var = input("Term 1 Variable: ")
+term2_coef = input("Term 2 Coefficient: ")
+term2_var = input("Term 2 Variable: ")
+
+term1 = str(term1_coef)+str(term1_var)
+term2 = str(term2_coef)+str(term2_var)
+print("Answer for the equation "+ term1 + term2 + " is:")
 
 coeff_add = int(term1_coef) + int(term2_coef)
 result = str(coeff_add) + str(term2_var)
